a003642f7120055b9c065b81c7f5211019a8f626
Craig Maloney <craig@decafbad.net>
2017-02-22 20:46:30 -0500

    Added the basic skeleton for Snake

---
+++ source/chapter2/snake.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys
+import pygame
+from pygame.locals import (
+    Rect)
+
+SCREENRECT = Rect(0, 0, 800, 600)
+BACKGROUND_COLOR = (0, 0, 0)
+
+
+def main():
+    running = True
+    pygame.init()
+    clock = pygame.time.Clock()
+    surface = pygame.display.set_mode(SCREENRECT.size)
+    surface.fill(BACKGROUND_COLOR)
+    while running:
+        pygame.display.update()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
+    pygame.quit()
+    sys.exit()
